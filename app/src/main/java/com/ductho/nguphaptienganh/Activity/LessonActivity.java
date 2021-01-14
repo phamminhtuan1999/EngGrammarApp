0bc22be53a36945d3c1287ec54e6470100b45e99
snootiecute <dangtiendat288@gmail.com>
2021-01-15 00:17:41 +0700

    fix loi mat toolbar

--- app/src/main/java/com/ductho/nguphaptienganh/Activity/LessonActivity.java
+++ app/src/main/java/com/ductho/nguphaptienganh/Activity/LessonActivity.java
@@ -6,20 +6,18 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.webkit.WebSettingsCompat;
-import androidx.webkit.WebViewFeature;
 
 import android.text.Html;
 import android.view.MenuItem;
 
+
 import com.ductho.nguphaptienganh.Adapter.CustomAdapter;
-import com.ductho.nguphaptienganh.Ads.AdBanner;
 import com.ductho.nguphaptienganh.Load.LoadBaiHoc;
 import com.ductho.nguphaptienganh.Model.BaiHoc;
 import com.ductho.nguphaptienganh.R;
-import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
 
@@ -29,16 +27,19 @@ public class LessonActivity extends AppCompatActivity {
     ArrayList<BaiHoc> arrayList;
     CustomAdapter adapter;
     String id, title;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         id = getIntent().getStringExtra("id");
         title = getIntent().getStringExtra("title");
-//        getSupportActionBar().setTitle(title);
+////       getSupportActionBar().setTitle(title);
         getSupportActionBar().setTitle((Html.fromHtml("<font color=\"#FFFFFF\">" + title + "</font>")));
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
         if (AppCompatDelegate.getDefaultNightMode() ==
--- app/src/main/java/com/ductho/nguphaptienganh/Activity/NoiDungActivity.java
+++ app/src/main/java/com/ductho/nguphaptienganh/Activity/NoiDungActivity.java
@@ -2,6 +2,7 @@ package com.ductho.nguphaptienganh.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.webkit.WebSettingsCompat;
 import androidx.webkit.WebViewFeature;
@@ -21,11 +22,14 @@ public class NoiDungActivity extends AppCompatActivity {
 
     String id, title;
     WebView wv;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_pdf);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         id = getIntent().getStringExtra("id");
